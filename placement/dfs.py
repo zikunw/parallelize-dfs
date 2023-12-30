1b590eff3e60cf699e218f5436de7881a1dcda81
Zikun Wang <68682076+zikunw@users.noreply.github.com>
2023-12-29 20:10:40 -0500

    naive multiprocess implementation

--- placement/dfs.py
+++ placement/dfs.py
@@ -2,7 +2,19 @@ from dfsUtil import *
 from singleThreadDFS import *
 from multiThreadDFS import *
 
+import multiprocessing as mp
+from enum import Enum
+import time
+
+class ProcessStatus(Enum):
+    UNINIT = 0
+    RUNNING = 1
+    WAITING = 2
+    HAS_TASK = 3
+
 def compareResult(result1, result2):
+    result1 = sorted(result1)
+    result2 = sorted(result2)
     if len(result1) != len(result2):
         return False
     
@@ -12,47 +24,139 @@ def compareResult(result1, result2):
     
     return True
 
-if __name__ == "__main__":
-    # ======== single thread dfs ======== 
-    print("Single thread dfs:")
-    # dfs operator stack
-    ops = []
-    ops.append(OP("source", 2))
-    ops.append(OP("map", 4))
-    ops.append(OP("sink", 2))
-
+# ======== single thread dfs ======== 
+def runSingle(ops):
+    
+    start = time.time()
+    
     # final results
     plans = []
     # current placement plan
     curPlace = []
 
     OuterDFS(ops, curPlace, plans)
-
-    # dfs results
-    for i in range(len(plans)):
-        print(i+1, plans[i])
+    
+    end = time.time()
         
-    # ======== multi thread dfs ========
-    print("\nMulti thread dfs:")
+    return plans, end-start
+
+# ======== multi thread dfs ========
+def run(idx, plans, plans_lock, processes_status, process_status_lock, processes_queue):
+    message_queue = processes_queue[idx]
+    print(f"Process {idx} started.")
+    process_status_lock.acquire()
+    processes_status[idx] = ProcessStatus.WAITING.value # finish and waiting for input
+    process_status_lock.release()
+    
+    while True:
+        msg = message_queue.get(block=True)
+        ops, curPlace = msg # For now, only one message type (for outer dfs)
+        
+        with process_status_lock:
+            processes_status[idx] = ProcessStatus.RUNNING.value
+            
+        print(f"Process {idx} received msg.")
+        MultiOuterDFS(ops, curPlace, plans, plans_lock, processes_status, process_status_lock, processes_queue)
+        print(f"Process {idx} finished.")
+        
+        with process_status_lock:
+            if processes_status[idx] == ProcessStatus.RUNNING.value:
+                processes_status[idx] = ProcessStatus.WAITING.value
+        
+def manager_run(processes_status, process_status_lock):
+    while True:
+        with process_status_lock:
+            stopped = True
+            for i in range(len(processes_status)):
+                if processes_status[i] != ProcessStatus.WAITING.value:
+                    stopped = False
+                    break
+            if stopped:
+                print("All processes finished.")
+                break
+        
+        time.sleep(0.01)
+
+def runMulti(ops, num_processes=4):
+    with mp.Manager() as manager:
+        start = time.time()
+        # final results
+        plans_lock = manager.Lock()
+        plans = manager.list()
+        
+        process_status_lock = manager.Lock()
+        processes_status = manager.Array('i', range(num_processes))
+        processes = []
+        processes_queue = []
+        for idx in range(num_processes):
+            processes_queue.append(manager.Queue())
+            processes.append(mp.Process(target=run, args=(
+                idx,
+                plans,
+                plans_lock,
+                processes_status,
+                process_status_lock,
+                processes_queue
+                )))
+            processes_status[idx] = ProcessStatus.UNINIT.value
+            
+        for p in processes:
+            p.start()
+            
+        # wait for all processes to be initialized
+        while True:
+            process_status_lock.acquire()
+            all_init = True
+            for i in range(len(processes_status)):
+                if processes_status[i] != ProcessStatus.WAITING.value:
+                    all_init = False
+                    break
+            if all_init:
+                process_status_lock.release()
+                break
+            process_status_lock.release()
+            time.sleep(0.01)
+            
+        print("All processes initialized.")
+        
+        # init for the first process
+        processes_status[0] = ProcessStatus.RUNNING.value
+        processes_queue[0].put((ops, []))
+        
+        # init a manager process
+        # once all processes are finished, the manager process will be finished
+        manager_process = mp.Process(target=manager_run, args=(processes_status, process_status_lock))
+        manager_process.start()
+        
+        manager_process.join()
+        
+        # kill all processes
+        for p in processes:
+            p.terminate()
+            
+        end = time.time()
+        
+        return list(plans), end-start
+
+if __name__ == "__main__":
     # dfs operator stack
     ops = []
     ops.append(OP("source", 2))
     ops.append(OP("map", 4))
     ops.append(OP("sink", 2))
     
-    # final results
-    multiplans = []
-    # current placement plan
-    curPlace = []
-    
-    MultiOuterDFS(ops, curPlace, multiplans)
-    
-    # dfs results
-    for i in range(len(multiplans)):
-        print(i+1, multiplans[i])
+    singleplans, singletime = runSingle(ops)
+    multiplans, multitime = runMulti(ops, 4)
         
     # ======== compare results ========
-    print("Compare results: ", compareResult(plans, multiplans))
+    print("Compare results: ", compareResult(singleplans, multiplans))
+    if not compareResult(singleplans, multiplans):
+        for i in range(len(singleplans)):
+            print("i = ", i)
+            print("Single: ", singleplans[i])
+            print("Multi: ", multiplans[i])
+    print("Single time: ", singletime)
+    print("Multi time: ", multitime)
 
     
 
--- placement/multiThreadDFS.py
+++ placement/multiThreadDFS.py
@@ -1,29 +1,45 @@
 import copy
 from dfsUtil import *
+from dfs import ProcessStatus
 
-def MultiOuterDFS(ops, curPlace, plans):
+def MultiOuterDFS(ops, curPlace, plans, plans_lock, processes_status, process_status_lock, processes_queue):
     
     #print("OuterDFS: ops = ", [op.name for op in ops], "curPlace = ", curPlace)
     
     if len(ops) == 0:
-        plans.append(copy.deepcopy(curPlace))
+        with plans_lock:
+            plans.append(copy.deepcopy(curPlace))
         return
 
     currentOP = ops[-1] #ops.pop()
     nodeList = []  # record temp placement for the current op
-    MultiInnerDFS(currentOP, ops[:-1], currentOP.parallelim, curPlace, nodeList, plans)
+    MultiInnerDFS(currentOP, ops[:-1], currentOP.parallelim, curPlace, nodeList, plans, plans_lock, processes_status, process_status_lock, processes_queue)
 
-def MultiInnerDFS(op, ops, leftTasks, curPlace, nodeList, plans):
+def MultiInnerDFS(op, ops, leftTasks, curPlace, nodeList, plans, plans_lock, processes_status, process_status_lock, processes_queue):
     
     #print("InnerDFS: op = ", op.name, "leftTasks = ", leftTasks, "curPlace = ", curPlace, "nodeList = ", nodeList)
     
     # Terminating condition
     if leftTasks <= 0:
+        
+        # if there are idle processes, send the task to the idle process
+        with process_status_lock:
+            idle_process_idx = -1
+            for j in range(len(processes_status)):
+                if processes_status[j] == ProcessStatus.WAITING.value:
+                    idle_process_idx = j
+                    break
+            if idle_process_idx != -1:
+                processes_queue[idle_process_idx].put((ops, curPlace + [strNodeList(nodeList)]))
+                processes_status[idle_process_idx] = ProcessStatus.HAS_TASK.value
+                return
+        
         # explore next op
-        MultiOuterDFS(ops, curPlace + [strNodeList(nodeList)], plans)
+        MultiOuterDFS(ops, curPlace + [strNodeList(nodeList)], plans, plans_lock, processes_status, process_status_lock, processes_queue)
         return
 
     upperBound = min(3, leftTasks)
+    
     for i in range(upperBound):
         taskPlaced = i+1
-        MultiInnerDFS(op, ops, leftTasks-taskPlaced, curPlace, nodeList + [op.name + str(taskPlaced)], plans)
+        MultiInnerDFS(op, ops, leftTasks-taskPlaced, curPlace, nodeList + [op.name + str(taskPlaced)], plans, plans_lock,  processes_status, process_status_lock, processes_queue)
